7baa8fffc7c3e169e451ef28b3076371c456b747
dhaneshawati <dawati66@gmail.com>
2023-02-15 10:34:35 +0530

    Frontend for Quora clone done

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import '../styles/App.css';
+
+import Quora from './Quora';
 const App = () => {
 
 
-  return (
+  return ( 
     <div id="main">
+     <Quora/>
     </div>
   )
 }
---
+++ src/components/Quora.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import '../styles/Quora.css'
+import Feed from './Feed'
+import Navbar from './Navbar'
+import Sidebar from './Sidebar'
+import Widget from './Widget'
+
+const Quora = () => {
+  return (
+    <div className='quora'>
+      <Navbar/>
+      <div className="quora_content">
+      <Sidebar />
+      <Feed/>
+      <Widget/>
+      </div>
+
+      
+    </div>
+  )
+}
+
+export default Quora
